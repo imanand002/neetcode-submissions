a0bb5c21974764ce4345d4ee5c6d8aa923594585
Mayank Anand <anand.work002@gmail.com>
2026-07-31 16:11:34 +0530

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def swap(a,b) -> None:
+        temp = a
+        a = b
+        b = t
+
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s = s.reverse()
+        
